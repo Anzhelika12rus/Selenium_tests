2d8856bda3967a514b3d6e31c777d765581c5605
Format1ka <isakova_akin@mail.ru>
2026-04-22 13:56:49 +0300

    Проверка элемента на странице, обработка NoSuchElementException

--- test_main_page.py
+++ test_main_page.py
@@ -13,4 +13,10 @@ def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 # pytest -v --tb=line --language=en test_main_page.py

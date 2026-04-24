a73b2e5b1a88939e7aaeed991c62bfdf1424f927
Format1ka <isakova_akin@mail.ru>
2026-04-24 11:07:12 +0300

    Изменены Переходы между страницами

--- test_main_page.py
+++ test_main_page.py
@@ -6,10 +6,16 @@ from .pages.locators import LoginPageLocators
 link = "http://selenium1py.pythonanywhere.com/"
 
 
+# def test_guest_can_go_to_login_page(browser):
+#     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page.open()  # открываем страницу
+#     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open() # открываем страницу
+    login_page = page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page.should_be_login_page()
 
 
 def go_to_login_page(browser):

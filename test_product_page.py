9799c3412b7cffa423a2e7e5cee7c2709dcd13b8
Format1ka <isakova_akin@mail.ru>
2026-04-28 17:42:19 +0300

    Последние изменения по коду

--- test_product_page.py
+++ test_product_page.py
@@ -49,8 +49,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, BASE_LINK)
     page.open()
     page.should_be_login_link()
 
@@ -58,8 +57,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
 @pytest.mark.xfail(reason="Must fall")
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, BASE_LINK)
     page.open()
     page.go_to_login_page()
 
